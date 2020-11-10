e060aa6ac01cc5e2e12df6e1099fde0a79c2327f
kamusone <benayad.kame@gmail.com>
2020-11-10 18:43:49 +0100

    test autogui mouse

---
+++ auto_mouse.py
@@ -0,0 +1,19 @@
+import pyautogui
+#import time
+# while True:
+#     if pyautogui.moveTo(10):
+#         time.sleep(10)
+#         pyautogui.press('esc')
+#         print("ok")
+#         # pyautogui.scroll(10)
+#     else:
+#         pyautogui.press('esc')
+
+#take a screen shot
+
+# pyautogui.screenshot("test screnshot whit pyautogui.png")
+import pyscreeze
+pyscreeze.screenshot("test screnshot whit pyautogui.png")
+
+
+    
